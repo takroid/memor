7ea5fd75da6d597a86805f0788ac7c23fdab6e54
takroid <sheetake1204@gmail.com>
2021-02-21 15:52:44 +0900

    first

---
+++ memor/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  memor
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
