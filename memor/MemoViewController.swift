eafbe5b8a36870e54447d434abb36c5db023e020
takroid <sheetake1204@gmail.com>
2021-02-23 19:00:50 +0900

    並び替え処理実装中

--- memor/MemoViewController.swift
+++ memor/MemoViewController.swift
@@ -42,6 +42,44 @@ class MemoViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return view.frame.size.height / 14
     }
+
+    //セルの編集許可
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
+    {
+        return true
+    }
+
+    //スワイプしたセルを削除　※arrayNameは変数名に変更してください
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            //削除対象のオブジェクト
+            let object: Memo = self.memoItem![(indexPath as NSIndexPath).row]
+            //Realm接続　データ削除
+            do {
+                let realm = try Realm()
+                try! realm.write {
+                    //TaskListオブジェクトの削除
+                    realm.delete(object)
+                }
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            } catch {
+                print("メモ削除失敗")
+            }
+        }
+    }
+    //描き編集中
+//    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+////        let todo = todos[sourceIndexPath.row]
+////        todos.remove(at: sourceIndexPath.row)
+////        todos.insert(todo, at: destinationIndexPath.row)
+//    }
+////セクション間の並び替えの許可
+//    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+//        if sourceIndexPath.section == proposedDestinationIndexPath.section {
+//            return proposedDestinationIndexPath
+//        }
+//        return sourceIndexPath
+//    }
     //returnキーが押された時に発動するメソッド
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
 
@@ -84,7 +122,7 @@ class MemoViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //TodaysTaskクラスに永続化されているデータを取りだす
+        //MEmoクラスに永続化されているデータを取りだす
         do {
             let realm = try Realm()
             memoItem = realm.objects(Memo.self)
@@ -97,9 +135,13 @@ class MemoViewController: UIViewController, UITableViewDelegate, UITableViewData
             action: #selector(MemoViewController.tapped(_:)))
         //tableviewへのtapを検知させる
         tapGesture.cancelsTouchesInView = false
-
+        //編集モード　セルの複数選択を可能に
+        MemoTableView.allowsMultipleSelectionDuringEditing = true
+        // ボタンの設定
+        navigationItem.rightBarButtonItem = editButtonItem
         MemoTableView.delegate = self
         MemoTableView.dataSource = self
+
         InputTextField.delegate = self
         tapGesture.delegate = self
         InputTextField.borderStyle = .none
